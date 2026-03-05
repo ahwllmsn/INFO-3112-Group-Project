82db81b7a6059d0f30bb2b4f784ca6078dad4727
Alyssa <alyssahwllmsn@gmail.com>
2026-03-05 13:51:33 -0500

    Added middleware for logging each request (from INFO-3139)

--- server/modules/api.js
+++ server/modules/api.js
@@ -7,6 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Custom middleware that logs each request.
+app.use((req, _res, next) => {
+    const timestamp = new Date(Date.now());
+    console.warn(`\n[${timestamp.toDateString()} ${timestamp.toTimeString()}] / ${timestamp.toISOString()}`);
+    console.log(req.method, req.hostname, req.path);
+    console.log('headers:', req.headers);
+    console.log('query:', req.query);
+    console.log('body:', req.body);
+    next();
+});
+
 // To-do: define endpoints.
 app.get('/test', (_req, res) => {
     res.sendFile("package.json", {root: '.'});
