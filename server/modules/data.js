2f4dae7e4d34b57c2f262385ab7127059a61f74c
Alyssa <alyssahwllmsn@gmail.com>
2026-03-05 14:55:06 -0500

    Added 2 methods for retrieving user info from database

--- server/modules/data.js
+++ server/modules/data.js
@@ -3,6 +3,7 @@ import * as db from './db.js';
 
 const DATABASE_NAME = "INFO-3112-Project";
 const USER_COLLECTION = "users";
+const MATCHES_COLLECTIOn = "matches"
 
 const addUser = async (user) => {
     let context = undefined;
@@ -15,6 +16,46 @@ const addUser = async (user) => {
     } finally {
         context?.close();
     }
+}
+
+// Retrieve only the log-in fields for a user with lookup using their email.
+const retrieveOneUserLogin = async (email) => {
+    let user = undefined;
+    let context = undefined;
+    try {
+        const query = {email};
+
+        // Project to only get id, email, password.
+        const projection = {
+            _id: 0,
+            email: 1,
+            password: 1
+        }
+
+        context = await db.initDatabase(env.DB_URI);
+        user = await db.findDocument(context, DATABASE_NAME, USER_COLLECTION, {}, projection);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        context?.close();
+    }
+    return user;
+
+}
+
+const retrieveOneUser = async (email) => {
+    let user = undefined;
+    let context = undefined;
+    try {
+        const query = {email};
+        context = await db.initDatabase(env.DB_URI);
+        user = await db.findDocument(context, DATABASE_NAME, USER_COLLECTION, {});
+    } catch (e) {
+        console.error(e);
+    } finally {
+        context?.close();
+    }
+    return user;
 }
 
 const retrieveAllUsers = async () => {
@@ -35,5 +76,7 @@ export {
     DATABASE_NAME,
     USER_COLLECTION, 
     addUser,
+    retrieveOneUserLogin,
+    retrieveOneUser,
     retrieveAllUsers
 }
