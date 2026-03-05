671d4339a0d1523fb19e3226d750f02627d06807
Alyssa <alyssahwllmsn@gmail.com>
2026-03-05 14:13:10 -0500

    Created client folder as a Vanilla Vite project

---
+++ client/src/counter.js
@@ -0,0 +1,12 @@
+// This was created in the default Vite build. Delete it if you want, just leaving it here (for now) as an example file. - Alyssa
+
+
+// export function setupCounter(element) {
+//   let counter = 0
+//   const setCounter = (count) => {
+//     counter = count
+//     element.innerHTML = `count is ${counter}`
+//   }
+//   element.addEventListener('click', () => setCounter(counter + 1))
+//   setCounter(0)
+// }
