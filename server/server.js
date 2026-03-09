5058d1207e16b2050ef67bfca5c080d07ff78faf
Alyssa <alyssahwllmsn@gmail.com>
2026-03-09 15:16:34 -0400

    Cleaned up old unused code from server.js

--- server/server.js
+++ server/server.js
@@ -1,28 +1,7 @@
-// import env from './modules/env.js';
-import { addUser, retrieveAllUsers } from './modules/data.js';
-
-// import { initDatabase, insertDocument, deleteDocument } from './modules/db.js';
-
 import { startServer } from './modules/api.js';
 
 const API_PORT = 9000;
-// await retrieveAllUsers();
 
 startServer(API_PORT);
 
-// let db = undefined;
-// try {
-//     let test_user = {name:"Example User", age: "22", gender:"female", location:"London, ON"};
-
-//     // let result = await addUser(test_user);
-
-//     let result = await retrieveAllUsers();
-
-//     console.log(result);
-    
-// } catch (e) {
-//     console.error(e);
-// } finally {
-//     db?.close();
-// }
 
